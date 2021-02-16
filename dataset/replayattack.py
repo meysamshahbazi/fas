744bb791458cd378d7e883b3f69c571c1a9bc415
meysam <mysm.shahbazi@gmail.com>
2021-02-16 16:10:35 +0330

    fix issues

--- dataset/replayattack.py
+++ dataset/replayattack.py
@@ -22,8 +22,7 @@ class ReplayAttack(FASDataset):
             vids = os.listdir(self.root+self.data_partion+vp)
             for v in vids:
                 my_dict[index] = {}
-                my_dict[index]['name'] = vp+v # such that with root+sub_dir+name we can load video
-                                
+                my_dict[index]['name'] = self.data_partion+vp+v # such that with root+name we can load video                
                 if vp == '/real/':  
                     my_dict[index]['person_id'] = int(v.split('_')[0][-3:])
                     my_dict[index]['real_or_spoof'] = 1 # 1 for real and 0 for spoof
@@ -31,12 +30,12 @@ class ReplayAttack(FASDataset):
                     my_dict[index]['lighting'] = v.split('_')[4]
                 else:
                     my_dict[index]['real_or_spoof'] = 0 # 1 for real and 0 for spoof
-                    my_dict[index]['PAI'] = v.split('_')[1] 
+                    my_dict[index]['PAI'] = v.split('_')[1] #TODO: fix it!
                     my_dict[index]['person_id'] = int(v.split('_')[2][-3:])
                     my_dict[index]['lighting'] = v.split('_')[6][:-4]
                 
                 
-                cap = cv2.VideoCapture(self.root+self.data_partion+my_dict[index]['name'] )
+                cap = cv2.VideoCapture(self.root+my_dict[index]['name'] )
                 frame_cnt = 0
                 while cap.isOpened():
                     ret,frame = cap.read()
@@ -59,3 +58,12 @@ class ReplayAttack(FASDataset):
         with open(self.root+self.data_partion+".json", "w") as outfile:  
             json.dump(my_dict, outfile) 
             
+
+
+
+
+if __name__ == "__main__":
+    root = '/home/meysam/Desktop/Replay-Attack/'
+    dataset = ReplayAttack(root,'train',4)
+    for i in dataset.datadict.keys():
+        print(dataset.datadict[i])
