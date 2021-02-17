bfbb901871075c49e2e141fde1f816620de1f44e
meysam <mysm.shahbazi@gmail.com>
2021-02-17 10:30:40 +0330

    ready for run in host

--- dataset/oulunpu.py
+++ dataset/oulunpu.py
@@ -11,13 +11,17 @@ import glob
 
 class OuluNPU(FASDataset):
     def crateJsonSummery(self):
-        sub_dir = self.data_partion[0].upper()+self.data_partion[1:]+'_files/'
-        vids = glob.glob(self.root+'/'+sub_dir+'*.avi')
+        sub_dir_dict = {'train':'Train_files/','test':'Test_files/','devel':'Dev_files/'} 
+
+        sub_dir = sub_dir_dict[self.data_partion]
+        vids = glob.glob(self.root+sub_dir+'*.avi')
 
         my_dict = {}
         for index,l in enumerate(vids):
             my_dict[index] = {} 
-            my_dict[index]['name'] = l.split('/')[-1]
+            
+            my_dict[index]['name'] = sub_dir+l.split('/')[-1]
+    
             my_dict[index]['person_id'] = int(l.split('/')[-1][:-4].split('_')[2])
             if int(l.split('/')[-1][:-4].split('_')[3]) ==1:
                 my_dict[index]['real_or_spoof'] = 1
@@ -29,7 +33,7 @@ class OuluNPU(FASDataset):
                 my_dict[index]['real_or_spoof'] = 0
                 my_dict[index]['PAI'] = 'replay'
                 
-            cap = cv2.VideoCapture(self.root+sub_dir+my_dict[index]['name'])
+            cap = cv2.VideoCapture(self.root+my_dict[index]['name'])
             frame_cnt = 0
             while cap.isOpened():
                 ret,frame = cap.read()
@@ -46,9 +50,13 @@ class OuluNPU(FASDataset):
                 my_dict[index]['nb_frame_total'] = frame_cnt + my_dict[index-1]['nb_frame_total'] 
             my_dict[index]['resolution'] = resolution
 
-
         with open(self.root+self.data_partion+".json", "w") as outfile:  
             json.dump(my_dict, outfile) 
 
 
 
+if __name__ == "__main__":
+    root = '/media/meysam/B42683242682E6A8/OULU-NPU/'
+    dataset = OuluNPU(root,'train')
+    dataset = OuluNPU(root,'test')
+    dataset = OuluNPU(root,'devel')
