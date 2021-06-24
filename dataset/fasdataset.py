fa0cfbe91d09902376e8ec1a55a7f584889246ac
meysam <mysm.shahbazi@gmail.com>
2021-06-24 07:32:14 -0400

    modify for using face img

--- dataset/fasdataset.py
+++ dataset/fasdataset.py
@@ -11,7 +11,7 @@ class FASDataset(data.Dataset):
     this is base class for FAS datasets
     other class must inheritance form this but impliment their own methods for cusotm functionality
     '''
-    def __init__(self,root,data_partion,batch_size=1,for_train=True):
+    def __init__(self,root,data_partion,batch_size=1,for_train=True,shape=(224,224)):
         self.root = root
         self.data_partion = data_partion
         self.for_train = for_train # for dev and test this is False
@@ -19,6 +19,7 @@ class FASDataset(data.Dataset):
         self.vid_list = []
         self.opened_vid = {}
         self.batch_size = batch_size
+        self.shape = shape
         if not os.path.isfile(self.root+self.data_partion+'.json'):
           print("Json doesnt Exist! try to create it and it would take a time!")
           self.crateJsonSummery()
@@ -38,10 +39,14 @@ class FASDataset(data.Dataset):
                     self.opened_vid = {}
                 self.opened_vid[self.vid_idx] = []
                 cap = cv2.VideoCapture(self.root+self.datadict[str(self.vid_idx)]['name'] )
+                face_locs = self.get_randomed_face_loc(self.vid_idx)
+                face_locs_idx = 0
                 while cap.isOpened():
                     ret,frame = cap.read()
                     if ret:
-                        frame = cv2.resize(frame, (320, 240))
+                        x1,y1,x2,y2 = face_locs[face_locs_idx]
+                        face_locs_idx +=1
+                        frame = frame[y1:y2,x1:x2,:]
                         self.opened_vid[self.vid_idx].append(frame)
                     else:
                         break
@@ -58,10 +63,14 @@ class FASDataset(data.Dataset):
                     
                 self.vid_list = []
                 cap = cv2.VideoCapture(self.root+self.datadict[str(self.vid_idx)]['name'] )
+                face_locs = self.get_randomed_face_loc(self.vid_idx)
+                face_locs_idx = 0
                 while cap.isOpened():
                     ret,frame = cap.read()
                     if ret:
-                        frame = cv2.resize(frame, (320, 240))
+                        x1,y1,x2,y2 = face_locs[face_locs_idx]
+                        face_locs_idx +=1
+                        frame = frame[y1:y2,x1:x2,:]
                         self.vid_list.append(frame)
                     else:
                         break
@@ -83,7 +92,7 @@ class FASDataset(data.Dataset):
         return x,y,id
 
     def clear_cache(self):#this must be called after end of each epoch in training
-      self.opened_vid = {}
+        self.opened_vid = {}
 
     def crateJsonSummery(self):
         raise NotImplementedError("Subclass must implement abstract method")
