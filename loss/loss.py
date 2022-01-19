b2fb268dd196e0041bd55e60d7215a23ea857e88
meysam <mysm.shahbazi@gmail.com>
2022-01-19 08:28:44 +0330

    refine dataset with mask

--- loss/loss.py
+++ loss/loss.py
@@ -4,10 +4,49 @@ import torch.nn.functional as F
 import itertools
 import math
 
+class ArcbId(nn.Module):
+
+    def __init__(self,alpha,beta,gamma,net,M =2,s=64,m=0.75):
+        super(ArcbId,self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.M = M
+        self.bce = nn.BCEWithLogitsLoss()
+        self.net =  net
+        self.m = m
+        self.s = s
+        self.gamma = gamma
+        self.pd = nn.PairwiseDistance(keepdim=  True)
+
+    def forward(self,outputs,classes,emb,ids):   
+
+        theta = torch.acos(outputs)
+        outs = classes*torch.cos(theta+self.m) + (1-classes)*torch.cos(theta-self.m)
+        outs.mul_(self.s)
+        l = self.gamma * self.bce(outs,classes)
+
+
+        idd = torch.combinations(ids.flatten(),2)
+        cll = torch.combinations(classes.flatten(),2)
+
+        idxx = torch.combinations(torch.arange(0,len(ids)),2)
+        
+        c1 = torch.logical_and(idd[:,0]!=idd[:,1],cll[:,0]==cll[:,1])# index for same class but diffrent id
+        c2 = torch.logical_and(idd[:,0]==idd[:,1],cll[:,0]!=cll[:,1]) # index for difrrent class label but same idreeeeeturn 
+        
+        c1_mask = torch.nonzero(c1)
+        c2_mask = torch.nonzero(c2)
+        
+        if c1.sum()>0:
+            l += self.alpha*( self.pd(emb[idxx[c1_mask,0]],emb[idxx[c1_mask,1]])  ).mean() 
+        if c2.sum()>0:
+            l += self.beta*(torch.max(torch.zeros_like(c2_mask),self.M-self.pd(emb[idxx[c2_mask,0]],emb[idxx[c2_mask,1]])).mean() )
+ 
+        return l
+
 class ArcB(nn.Module):
-    def __init__(self,net, m=0.5,s=1.0):
+    def __init__(self,net, m=0.75,s=64):
         super(ArcB, self).__init__()
-        print("*************")
         self.m = m 
         self.s = s
         self.net = net
@@ -15,11 +54,10 @@ class ArcB(nn.Module):
 
 
     def forward(self,outputs,classes,emb,ids):
-        w = list(self.net.parameters())[-1]
-        scale = torch.norm(w)*torch.norm(emb,dim=1)
-        scale = scale.unsqueeze(dim=1)
-        theta = torch.acos(emb.matmul(w.T)/scale)
-        outs = classes*scale*torch.cos(theta+self.m) + (1-classes)*scale*torch.cos(theta-self.m)
+        theta = torch.acos(outputs)
+        outs = classes*torch.cos(theta+self.m) + (1-classes)*torch.cos(theta-self.m)
+        outs.mul_(self.s)
+
         return self.bce(outs,classes)
 
 
@@ -32,24 +70,6 @@ class BCEWithLogits(nn.Module):
     def forward(self,outputs,classes,emb,ids):
         return self.criterion(outputs, classes)
 
-'''
-class IdBce(nn.Module):
-    def __init__(self,alpha=0.5,M=0.5):
-        super(IdBce,self).__init__()
-        self.alpha = alpha
-        self.M = M
-        self.bce =  nn.BCEWithLogitsLoss()
-
-    def forward(self,outputs,classes,emb,ids):
-        l = 0
-        emb = F.normalize(emb)
-        for i,j in itertools.combinations(range(len(ids)), 2):
-            l+= 0.5*int(ids[i]==ids[j]and classes[i]!=classes[j])*((emb[i]-emb[j])**2).sum()
-            l+=int(ids[i]!=ids[j]and classes[i]==classes[j])*torch.max(torch.FloatTensor([0,self.M-(torch.abs(emb[i]-emb[j])).sum()]))
-        #print("l is:",l)
-        return self.bce(outputs, classes) + self.alpha*l
-
-'''
 
 class IdBce(nn.Module):
     def __init__(self,alpha=0.5,M=0.5):
@@ -59,75 +79,75 @@ class IdBce(nn.Module):
         self.bce =  nn.BCEWithLogitsLoss()
 
     def forward(self,outputs,classes,emb,ids):
-            
-        idx1 = [] #index for same id but diffrent class label
-        idx2 = [] #index for same class label but diffrent id
+
         l = 0 
-        emb = F.normalize(emb)
+        idd = torch.combinations(ids.flatten(),2)
+        cll = torch.combinations(classes.flatten(),2)
 
-        idd = torch.combinations(ids.reshape(-1),2)
-        cll = torch.combinations(classes.reshape(-1),2)
         idxx = torch.combinations(torch.arange(0,len(ids)),2)
-        c1 = torch.logical_and(idd[:,0]!=idd[:,1],cll[:,0]==cll[:,1]).reshape(-1,1) # index for same class but diffrent id
-        c2 = torch.logical_and(idd[:,0]==idd[:,1],cll[:,0]!=cll[:,1]) # index for difrrent class label but same id
-        emb_dif = emb[idxx[:,0]]-emb[idxx[:,1]]
-        if c1.sum() > 0: 
-            l += torch.norm((c1*emb_dif),dim=1).sum()/c1.sum()
-        if c2.sum() > 0:
-            l += ( c2*torch.max(torch.zeros(len(idxx),device=emb.device),self.M-torch.norm((emb_dif),2,dim=1)) ).sum()/(c2.sum())
+        
+        c1 = torch.logical_and(idd[:,0]!=idd[:,1],cll[:,0]==cll[:,1])# index for same class but diffrent id
+        c2 = torch.logical_and(idd[:,0]==idd[:,1],cll[:,0]!=cll[:,1]) # index for difrrent class label but same idreeeeeturn 
+        
+        c1_mask = torch.nonzero(c1)
+        c2_mask = torch.nonzero(c2)
+        
+        if c1.sum()>0:
+            l += self.alpha*( self.pd(emb[idxx[c1_mask,0]],emb[idxx[c1_mask,1]])  ).mean() 
+        if c2.sum()>0:
+            l += self.beta*(torch.max(torch.zeros_like(c2_mask),self.M-self.pd(emb[idxx[c2_mask,0]],emb[idxx[c2_mask,1]])).mean() )
+
         lbce= self.bce(outputs, classes)
-        # print(lbce," | ",l)
+
         return lbce + self.alpha*l
 
 
+""" class ArcbTPId(nn.Module):
 
-
-class ArcbId(nn.Module):
-
-    def __init__(self,alpha,beta,net,M = 0.5,m=0.5):
-        super(ArcbId,self).__init__()
+    def __init__(self,alpha,beta,gamma,net,M = 0.5,m=0.5):
+        super(ArcbTPId,self).__init__()
         self.alpha = alpha
         self.beta = beta
         self.M = M
         self.bce = nn.BCEWithLogitsLoss()
+        # self.bce = nn.BCELoss()
         self.net =  net
         self.m = m
+        self.s = 2.6
+        self.gamma = gamma
+        
+        self.triplet_loss = nn.TripletMarginLoss(margin=1.0, p=2)
+
+        # self.pd2 = nn.PairwiseDistance(keepdim=  False)
+        self.cs = nn.CosineSimilarity()
         print("ArcB loss with Person ID based loss")
 
     def forward(self,outputs,classes,emb,ids):
-        w = list(self.net.parameters())[-1]
+        # w = list(self.net.parameters())[-1]
+        w = self.net.head.weight
+        # scale = torch.norm(w)*torch.norm(emb,dim=1)
         scale = torch.norm(w)*torch.norm(emb,dim=1)
         scale = scale.unsqueeze(dim=1)
-        theta = torch.acos(emb.matmul(w.T)/scale)
+        
+        
+        theta = torch.acos(self.cs(emb,w)).unsqueeze(dim=1)
+        
+        outs = classes*torch.cos(theta+self.m) + (1-classes)*torch.cos(theta-self.m)
+        outs = self.s*outs
+    
+        idd = torch.combinations(ids.reshape(-1),3)
+        cll = torch.combinations(classes.reshape(-1),3)
+        idxx = torch.combinations(torch.arange(0,len(ids)),3)
+        mask = torch.logical_and(cll[:,0]==cll[:,1],idd[:,0]!=idd[:,1]).logical_and(torch.logical_and(cll[:,0]!=cll[:,2],idd[:,0]==idd[:,2]))
+        mask = torch.nonzero(mask).flatten()
+        anchor = emb[idxx[mask,0]]
+        positive = emb[idxx[mask,1]]
+        negative = emb[idxx[mask,2]]
+        # print(self.triplet_loss(anchor,positive,negative) )
+        if len(mask)>0:
+            return self.alpha*self.triplet_loss(anchor,positive,negative) + self.beta*self.bce(outs,classes)
+        else:
+            return self.beta*self.bce(outs,classes)
+ """
 
-        outs = classes*scale*torch.cos(theta+self.m) + (1-classes)*scale*torch.cos(theta-self.m)
-        l = 0
-
-        emb = F.normalize(emb)
-
-        idd = torch.combinations(ids.reshape(-1),2)
-        cll = torch.combinations(classes.reshape(-1),2)
-        idxx = torch.combinations(torch.arange(0,len(ids)),2)
-        c1 = torch.logical_and(idd[:,0]!=idd[:,1],cll[:,0]==cll[:,1]).reshape(-1,1) # index for same class but diffrent id
-        c2 = torch.logical_and(idd[:,0]==idd[:,1],cll[:,0]!=cll[:,1]) # index for difrrent class label but same id
-        emb_dif = emb[idxx[:,0]]-emb[idxx[:,1]] 
-        l1 = 0
-        l2 = 0
-        if c1.sum() > 0: 
-            l1 = torch.norm((c1*emb_dif),dim=1).sum()/c1.sum()
-        if c2.sum() > 0:
-            # l2 = ( c2*torch.max(torch.zeros(len(idxx),device=emb.device),self.M-torch.norm((emb_dif),2,dim=1)) ).sum()/(c2.sum())
-            l2 =  (c2*torch.max(torch.zeros(len(idxx),device=emb.device),self.M-torch.norm((emb_dif),2,dim=1))).sum()/c2.sum()
-
-        # if c1.sum() > 0: 
-        #     emb_dif1 = emb[idxx[c1][:,0]]-emb[idxx[c1][:,1]]
-        #     l1 = torch.norm((emb_dif1),dim=1).sum()/c1.sum()
-        # if c2.sum() > 0:
-        #     emb_dif2 = emb[idxx[c2][:,0]]-emb[idxx[c2][:,1]]
-        #     l2 =  (torch.max(torch.zeros(c2.sum(),device=emb.device),self.M-torch.norm((emb_dif2),2,dim=1))).sum()/c2.sum()
-        # print(l1,"|",l2)
-        lbce = self.bce(outs,classes)
-        # print(l1,"|",l2,"|",lbce)
-        return lbce + self.alpha*l1 + self.beta*l2
-
-
+ 
